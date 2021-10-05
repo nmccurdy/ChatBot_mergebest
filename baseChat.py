d7ac2efc997fa13d4b5d9ef9fab51a88bc15ad47
Neil McCurdy <neil.mccurdy@gmail.com>
2021-10-05 02:59:10 +0000

    Added notes about how to do this using plugins instead

--- baseChat.py
+++ baseChat.py
@@ -1,3 +1,11 @@
+# eventually we may want to implement a plugin manager like this:
+# https://www.guidodiepen.nl/2019/02/implementing-a-simple-plugin-framework-in-python/
+#
+# this will make it easier for students to add chatObjects without having to call
+# addChatObject() and without us ever having to modify the imports in main.py.  
+# The problem, though, is that the chatObjects would all have to be
+# initialized using the same parameters.
+
 CHAT_OBJECTS = []
 
 def addChatObject(chatObject):
